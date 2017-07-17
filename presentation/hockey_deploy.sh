ef4522b4f4176ec4b4c15b10ce3154a78e466a4d
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-07-17 18:35:56 +0200

    Added deploy script

---
+++ presentation/hockey_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+releaseVersion=$(curl -H "X-HockeyAppToken: 5bd88f774e5f41e9ad40f66d28926d01" https://rink.hockeyapp.net/api/2/apps/c6720c02a0b943b588aac446fd995a34/app_versions | tr ',' '\n' | head -n 1 | tr '[' '\n' | grep '{"version":' | cut -b 13- | rev | cut -b 2- | rev)
+developVersion=$(curl -H "X-HockeyAppToken: 5bd88f774e5f41e9ad40f66d28926d01" https://rink.hockeyapp.net/api/2/apps/c6720c02a0b943b588aac446fd995a34/app_versions | tr ',' '\n' | head -n 1 | tr '[' '\n' | grep '{"version":' | cut -b 13- | rev | cut -b 2- | rev)
+
+nothingToDo="The version of the application is not greater than the one in HockeyApp. Nothing to do."
+
+echo "Current version on hockey app is: $V"
+echo "Current app version is: $1"
+echo "Current type of teh application: $2"
+
+if [ $2 = "debug" ]
+then
+    if [ $developVersion -lt $1 ]
+    then
+        ../gradlew uploadDebugToHockeyApp
+    else
+        echo $nothingToDo
+    fi
+elif [ $releaseVersion -lt $1 ]
+then
+    ../gradlew uploadReleaseToHockeyApp
+else
+    echo $nothingToDo
+fi
+
+
+
+
+
+
