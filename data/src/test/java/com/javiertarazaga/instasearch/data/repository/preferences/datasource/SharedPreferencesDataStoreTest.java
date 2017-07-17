70f4bfdb7e8a73ecbdd216f1ef1e4510ccab8e10
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-07-17 17:06:26 +0200

    Added unitary test for preferences

--- data/src/test/java/com/javiertarazaga/instasearch/data/repository/preferences/datasource/SharedPreferencesDataStoreTest.java
+++ data/src/test/java/com/javiertarazaga/instasearch/data/repository/preferences/datasource/SharedPreferencesDataStoreTest.java
@@ -1,63 +1,60 @@
-///**
-// * Copyright (C) 2017 Javier Tarazaga Open Source Project
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//package com.javiertarazaga.instasearch.data.repository.preferences.datasource;
-//
-//import com.javiertarazaga.instasearch.data.cache.UserCache;
-//import com.javiertarazaga.instasearch.data.entity.UserEntity;
-//import com.javiertarazaga.instasearch.data.net.RestApi;
-//import com.javiertarazaga.instasearch.data.repository.user.datasource.CloudUserDataStore;
-//import io.reactivex.Observable;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.verify;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class SharedPreferencesDataStoreTest {
-//
-//  private static final int FAKE_USER_ID = 765;
-//
-//  private CloudUserDataStore cloudUserDataStore;
-//
-//  @Mock private RestApi mockRestApi;
-//  @Mock private UserCache mockUserCache;
-//
-//  @Before
-//  public void setUp() {
-//    cloudUserDataStore = new CloudUserDataStore(mockRestApi, mockUserCache);
-//  }
-//
-//  @Test
-//  public void testGetUserEntityListFromApi() {
-//    cloudUserDataStore.userEntityList();
-//    verify(mockRestApi).userEntityList();
-//  }
-//
-//  @Test
-//  public void testGetUserEntityDetailsFromApi() {
-//    UserEntity fakeUserEntity = new UserEntity();
-//    Observable<UserEntity> fakeObservable = Observable.just(fakeUserEntity);
-//    given(mockRestApi.userEntityById(FAKE_USER_ID)).willReturn(fakeObservable);
-//
-//    cloudUserDataStore.userEntityDetails(FAKE_USER_ID);
-//
-//    verify(mockRestApi).userEntityById(FAKE_USER_ID);
-//  }
-//}
+/**
+ * Copyright (C) 2017 Javier Tarazaga Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.javiertarazaga.instasearch.data.repository.preferences.datasource;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class) public class SharedPreferencesDataStoreTest {
+
+  private static final int FAKE_DISTANCE = 1000;
+
+  private SharedPreferencesDataStore sharedPreferencesDataStore;
+
+  @Mock private SharedPreferences mockSharedPreferences;
+  @Mock private SharedPreferences.Editor mockSharedPreferencesEditor;
+
+  @Before public void setUp() {
+    this.sharedPreferencesDataStore = new SharedPreferencesDataStore(this.mockSharedPreferences);
+  }
+
+  @Test public void testGetMaxDistanceFromSharedPreferences() {
+    given(this.mockSharedPreferences.getInt(anyString(), anyInt())).willReturn(FAKE_DISTANCE);
+
+    this.sharedPreferencesDataStore.getDistance();
+    verify(this.mockSharedPreferences).getInt(anyString(), anyInt());
+  }
+
+  @SuppressLint("CommitPrefEdits") @Test public void testSaveMaxDistanceIntoSharedPreferences() {
+    given(this.mockSharedPreferences.edit()).willReturn(this.mockSharedPreferencesEditor);
+    given(this.mockSharedPreferencesEditor.putInt(anyString(), anyInt())).willReturn(
+        this.mockSharedPreferencesEditor);
+
+    this.sharedPreferencesDataStore.saveDistance(FAKE_DISTANCE);
+    verify(mockSharedPreferencesEditor).putInt("distance", FAKE_DISTANCE);
+    verify(mockSharedPreferencesEditor).apply();
+  }
+}
