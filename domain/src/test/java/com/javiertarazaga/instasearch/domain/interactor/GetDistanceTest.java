c9c5bdf25323d7844987cc7e04dd7bc3032987a4
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-07-17 15:49:20 +0200

    Added tests for interactors

---
+++ domain/src/test/java/com/javiertarazaga/instasearch/domain/interactor/GetDistanceTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2017 Javier Tarazaga Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.javiertarazaga.instasearch.domain.interactor;
+
+import com.javiertarazaga.instasearch.domain.executor.PostExecutionThread;
+import com.javiertarazaga.instasearch.domain.executor.ThreadExecutor;
+import com.javiertarazaga.instasearch.domain.repository.PreferencesRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetDistanceTest {
+
+  private GetDistance getDistance;
+
+  @Mock private PreferencesRepository mockPreferencesRepository;
+  @Mock private ThreadExecutor mockThreadExecutor;
+  @Mock private PostExecutionThread mockPostExecutionThread;
+
+  @Rule public ExpectedException expectedException = ExpectedException.none();
+
+  @Before
+  public void setUp() {
+    getDistance = new GetDistance(mockPreferencesRepository, mockThreadExecutor,
+        mockPostExecutionThread);
+  }
+
+  @Test
+  public void testGetUserDetailsUseCaseObservableHappyCase() {
+    getDistance.buildUseCaseObservable(null);
+
+    verify(mockPreferencesRepository).getDistance();
+    verifyNoMoreInteractions(mockPreferencesRepository);
+    verifyZeroInteractions(mockPostExecutionThread);
+    verifyZeroInteractions(mockThreadExecutor);
+  }
+}
