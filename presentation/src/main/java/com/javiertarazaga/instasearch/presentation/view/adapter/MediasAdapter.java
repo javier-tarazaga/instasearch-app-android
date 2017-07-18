5fb3d21e3def4c9430035fc25ebe149644f7ecfe
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-07-18 21:19:09 +0200

    Added picasso and fixed layout for list

--- presentation/src/main/java/com/javiertarazaga/instasearch/presentation/view/adapter/MediasAdapter.java
+++ presentation/src/main/java/com/javiertarazaga/instasearch/presentation/view/adapter/MediasAdapter.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2014 android10.org. All rights reserved.
+ *
  * @author Fernando Cejas (the android10 coder)
  */
 package com.javiertarazaga.instasearch.presentation.view.adapter;
@@ -15,6 +16,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.javiertarazaga.instasearch.presentation.R;
 import com.javiertarazaga.instasearch.presentation.model.MediaModel;
+import com.squareup.picasso.Picasso;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -24,6 +26,8 @@ import javax.inject.Inject;
  * Adapter that manages a collection of {@link MediaModel}.
  */
 public class MediasAdapter extends RecyclerView.Adapter<MediasAdapter.MediaViewHolder> {
+
+  private final Context context;
 
   public interface OnItemClickListener {
     void onMediaItemClicked(MediaModel mediaModel);
@@ -35,6 +39,7 @@ public class MediasAdapter extends RecyclerView.Adapter<MediasAdapter.MediaViewH
   private OnItemClickListener onItemClickListener;
 
   @Inject MediasAdapter(Context context) {
+    this.context = context;
     this.layoutInflater =
         (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     this.mediaCollection = Collections.emptyList();
@@ -52,6 +57,22 @@ public class MediasAdapter extends RecyclerView.Adapter<MediasAdapter.MediaViewH
   @Override public void onBindViewHolder(MediaViewHolder holder, final int position) {
     final MediaModel mediaModel = this.mediaCollection.get(position);
     holder.tv_username.setText(mediaModel.getUser().getUsername());
+
+    // TODO - Setup Picasso through DI instead. U2020 project style
+    Picasso.with(this.context)
+        .load(mediaModel.getUser().getProfilePicture())
+        .into(holder.iv_user_image);
+    Picasso.with(this.context)
+        .load(mediaModel.getImages().getLowResolution().getUrl())
+        .into(holder.iv_post);
+
+    if (mediaModel.getCaption() != null && mediaModel.getCaption().getText() != null) {
+      holder.tv_post_comment.setVisibility(View.VISIBLE);
+      holder.tv_post_comment.setText(mediaModel.getCaption().getText());
+    } else {
+      holder.tv_post_comment.setVisibility(View.GONE);
+    }
+
     holder.itemView.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         if (MediasAdapter.this.onItemClickListener != null) {
@@ -71,7 +92,7 @@ public class MediasAdapter extends RecyclerView.Adapter<MediasAdapter.MediaViewH
     this.notifyDataSetChanged();
   }
 
-  public void setOnItemClickListener (OnItemClickListener onItemClickListener) {
+  public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
     this.onItemClickListener = onItemClickListener;
   }
 
